5c38cea3025ea4bad5b8617c98f962640ee5a906
gurrrrrrett3 <61361654+gurrrrrrett3@users.noreply.github.com>
2021-05-20 21:21:25 -0400

    Update countdown.js

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("May 28, 2021 15:00:00").getTime();
+var countDownDate = new Date("May 21, 2021 16:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
