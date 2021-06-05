b0c39268b8fcf944c1de0e9a1493a6af91be03c4
gurrrrrrett3 <61361654+gurrrrrrett3@users.noreply.github.com>
2021-06-05 15:10:37 -0400

    More Secrets

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,6 +1,14 @@
 // Set the date we're counting down to
 var countDownDate = new Date("June 15, 2021 16:00:00 EST").getTime();
 
+const secret = [
+  "Create",
+  "Rats",
+  "Quark",
+  "Ars Magica",
+  "Tinker's Construct"
+]
+
 // Update the count down every 1 second
 var x = setInterval(function() {
 
@@ -21,6 +29,14 @@ var x = setInterval(function() {
   document.getElementById("h").innerHTML = hours + " hours"
   document.getElementById("m").innerHTML = minutes + " minutes" 
   document.getElementById("s").innerHTML = seconds + " seconds"
+
+  //display secrets
+
+  if (Math.random() > 0.75) {
+    const choice = secret[Math.floor(Math.random() * secret.length)]
+
+    console.error(choice)
+  }
 
   // If the count down is finished, write some text
   if (distance < 0) {
