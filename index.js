46a55092d0a849f513f175efa9d9f46b43136b29
gurrrrrrett3 <garrettkeneau@gmail.com>
2021-06-26 13:52:17 -0400

    Update URLs for new server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const config = require("./config.json")
-const { port, user, pass } = config
+const { port } = config
 const link = require("./routers/link")
 const path = require("path")
 
@@ -18,26 +18,6 @@ app.use("/link", link)
 app.get("/", (req, res) => {
 
     return res.sendFile(path.join(__dirname,"pages/home.html"))
-})
-
-app.get("/admin/home", (req, res) => {
-
-        return res.sendFile(path.join(__dirname,"pages/admin.html")) 
-})
-
-//admin stuff
-
-app.get("/admin/login", (req,res) => {
-
-    const query = req.query
-
-    if (query.user == user && query.pass == pass) {
-        res.redirect("/admin/home")
-        
-    } else {
-        res.redirect(403, "/")
-    }
-
 })
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
